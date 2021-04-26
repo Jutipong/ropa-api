36ff5726aab25d4413035e9049fb96aa2df5d701
T480s <techcom.ec20@gmail.com>
2021-04-27 01:19:36 +0700

    Update: Ef

--- Entities/DdContextTcrb/TcrbContext.cs
+++ Entities/DdContextTcrb/TcrbContext.cs
@@ -23,6 +23,15 @@ namespace WebApi.Entities.DdContextTcrb
         public virtual DbSet<MsQuestion> MsQuestion { get; set; }
         public virtual DbSet<QuestionGroup> QuestionGroup { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-TTFGOQA;Initial Catalog=Ropa;Integrated Security=True");
+            }
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "Thai_CI_AS");
