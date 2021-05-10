8ae80b4f9bf0bf804ed81d7d4f5e8033c2b0cdbe
T480s <techcom.ec20@gmail.com>
2021-05-10 16:10:59 +0700

    Update Code

---
+++ Services/MsQuestion/IMsQuestionService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Services.MsQuestion
+{
+    public interface IMsQuestionService
+    {
+        public ResponseModels<Entities.Models.MsGroup> Inquiry(GroupReqModel req);
+        public ResponseModels<Entities.Models.MsGroup> Create(List<Entities.Models.MsGroup> msGroups);
+        public ResponseModel Update(List<Entities.Models.MsGroup> msGroups);
+        public ResponseModel Delete(List<Entities.Models.MsGroup> msGroups);
+    }
+}
